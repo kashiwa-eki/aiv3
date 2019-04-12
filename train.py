73df43c3a449052a872eed868dc3abd2e004e4b6
kashiwa-eki <Jason@Jasons-iMac.local>
2019-04-12 15:56:00 -0400

    version 3.1

--- train.py
+++ train.py
@@ -1,3 +1,16 @@
+#  AI Postmodule Assignment - Predict last two words of review
+#  MSBA Spring 2019
+#  April 29, 2019
+#  Study Group 17
+#  - Madi Benjamin
+#  - Jason Inzer
+#  - Shivika Mehta
+#  - Ann Michael
+#
+#  Code adapted from multiple LSTM examples found online
+#  Special thanks to: https://machinelearningmastery.com/develop-word-based-neural-language-models-python-keras/
+
+
 from numpy import array
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
@@ -7,28 +20,25 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Embedding
 import random
+import numpy
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+seed = 7
+numpy.random.seed(seed)
+
 
 # load doc into memory
 def load_doc(filename):
-	# open the file as read only
 	file = open(filename, 'r')
-	# read all text
 	text = file.read()
-	# close the file
 	file.close()
 	return text
-
-# save tokens to file, one dialog per line
-def save_doc(lines, filename):
-	data = '\n'.join(lines)
-	file = open(filename, 'w')
-	file.write(data)
-	file.close()
 
 # generate a sequence from a language model
 def generate_seq(model, tokenizer, max_length, seed_text, n_words):
 	in_text = seed_text
-	# generate a fixed number of words
+	# generate a fixed number of words (2 for our example)
 	for _ in range(n_words):
 		# encode the text as integer
 		encoded = tokenizer.texts_to_sequences([in_text])[0]
@@ -46,13 +56,9 @@ def generate_seq(model, tokenizer, max_length, seed_text, n_words):
 		in_text += ' ' + out_word
 	return in_text
  
-# source text
+# source data
 in_filename = "review.dat"
 doc = load_doc(in_filename)
-
-#with open(in_filename, "r") as f:
-#    doc = f.read().split('\n')
-
 
 # prepare the tokenizer on the source text
 tokenizer = Tokenizer()
@@ -62,55 +68,52 @@ tokenizer.fit_on_texts([doc])
 vocab_size = len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
 
-# create line-based sequences
+# convert text to integers for sequences
+encoded = tokenizer.texts_to_sequences([doc])[0]
 sequences = list()
-for line in doc.split('\n'):
-	encoded = tokenizer.texts_to_sequences([line])[0]
-	sequence = encoded[:len(encoded)-1]
-	sequences.append(sequence)
-	sequence = encoded[:]
-	sequences.append(sequence)
-#	for i in range(1, len(encoded)):
-#		sequence = encoded[:i+1]
-#		sequences.append(sequence)
+# generate sequence 2 words in -> 1 word out
+for i in range(2, len(encoded)):
+    sequence = encoded[i-2:i+1]
+    sequences.append(sequence)
+# generate sequence 3 words in -> 1 word out
+for i in range(3, len(encoded)):
+    sequence = encoded[i-3:i+1]
+    sequences.append(sequence)
+# generate sequence 4 words in -> 1 word out
+for i in range(4, len(encoded)):
+    sequence = encoded[i-4:i+1]
+    sequences.append(sequence)
+# generate sequence 5 words in -> 1 word out
+for i in range(5, len(encoded)):
+    sequence = encoded[i-5:i+1]
+    sequences.append(sequence)
+
 print('Total Sequences: %d' % len(sequences))
 
-# save sequences to file
-#out_filename = 'review_sequences.txt'
-#save_doc(sequences, out_filename)
-
-#print(sequences)
-
-# pad input sequences
+# take care of variable length sequences by adding padding
 max_length = max([len(seq) for seq in sequences])
 sequences = pad_sequences(sequences, maxlen=max_length, padding='pre')
 print('Max Sequence Length: %d' % max_length)
 
-print(sequences)
-
-# save sequences to file
-#out_filename = 'review_sequences.txt'
-#save_doc(sequences, out_filename)
-
+# assign sequences to input/output elements
 sequences = array(sequences)
 X, y = sequences[:,:-1],sequences[:,-1]
 y = to_categorical(y, num_classes=vocab_size)
 
-# define model
+# create model with single hidden LSTM layer with 128 memory units
 model = Sequential()
-model.add(Embedding(vocab_size, 10, input_length=max_length-1))
-model.add(LSTM(50))
+model.add(Embedding(vocab_size, 50, input_length=max_length-1))
+model.add(LSTM(128))
 model.add(Dense(vocab_size, activation='softmax'))
 print(model.summary())
 
-# compile network
+# compile model and evaluate using accuracy
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# fit network
-model.fit(X, y, epochs=10, verbose=2)
+# fit model using 80% of data for training and 20% for testing. 100 epochs and batch size of 64.
+model.fit(X, y, validation_split=0.2, batch_size=64, epochs=100, verbose=2)
+
+# save model
 model.save('lstm.h5')
 
-#evaluate model
-print(generate_seq(model, tokenizer, max_length-1, 'good sounding strings and they', 2))
-print(generate_seq(model, tokenizer, max_length-1, 'unwound strings were a tiny bit', 2))
 
